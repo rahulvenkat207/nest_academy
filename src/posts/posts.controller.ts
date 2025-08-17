7dbed50eae03d2bfad2e64ebba84cbc0fdfd9b7c
rahulvenkat207 <rahulvenkat207@gmail.com>
2025-08-17 19:44:01 +0530

    Implemented ORM Operations

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dtos/createPost.dto';
+import { UpdatePostDto } from './dtos/updatePost.dto';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+    constructor(
+        private readonly postService : PostsService
+    ){}
+
+    @Get()
+    getAllPosts(){
+       return this.postService.getAllPosts();
+    }
+
+    @Get(':id')
+    getPostById(@Param('id', ParseIntPipe) id:number){
+        return  this.postService.getPostById(id);
+    }
+
+    @Post()
+    async createPost(@Body() post : CreatePostDto){
+        return this.postService.createPost(post);
+    }
+
+    @Put(':id')
+    async replacePost(@Param('id',ParseIntPipe)id:number, @Body() post : UpdatePostDto){
+        return  this.postService.replacePost(id,post);
+    }
+
+    @Delete(':id')
+    async deletePost(@Param('id',ParseIntPipe) id:number){
+        return this.postService.deletePost(id);
+    }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dtos/createPost.dto';
+import { UpdatePostDto } from './dtos/updatePost.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>,
+  ) {}
+
+  async getAllPosts() {
+    return this.postsRepository.find();
+  }
+
+  async getPostById(id: number) {
+    const post = await this.postsRepository.findOne({ where: { id } });
+    if (post) {
+      return post;
+    }
+    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+  }
+
+  async createPost(post: CreatePostDto) {
+    const newPost = this.postsRepository.create(post);
+    return await this.postsRepository.save(newPost);
+  }
+
+  async replacePost(id: number, post: UpdatePostDto) {
+    const existingPost = await this.postsRepository.findOne({ where: { id } });
+    if (!existingPost) {
+      throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    }
+    // replace: overwrite everything
+    await this.postsRepository.update(id, post);
+    return this.postsRepository.findOne({ where: { id } });
+  }
+
+  async deletePost(id: number) {
+    const result = await this.postsRepository.delete(id);
+    if (result.affected === 0) {
+      throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    }
+    return { deleted: true };
+  }
+}
