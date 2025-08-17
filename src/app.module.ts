86b586d3fe3b47b02daf846d834e8a5886873a13
rahulvenkat207 <rahulvenkat207@gmail.com>
2025-08-17 15:02:09 +0530

    new project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { TweetModule } from './tweet/tweet.module';
+
 
 @Module({
-  imports: [UsersModule, TweetModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
